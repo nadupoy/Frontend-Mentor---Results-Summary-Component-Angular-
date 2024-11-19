63b1430b5131019b606ff33ac1f180678f5ad408
nadupoy <gnsampao@gmail.com>
2024-11-19 12:33:23 +0300

    Deleted 'RouterOutlet'. Imported 'ResultComponent', 'SummaryComponent' & 'FooterComponent'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ResultComponent } from './result/result.component';
+import { SummaryComponent } from './summary/summary.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ResultComponent, SummaryComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
