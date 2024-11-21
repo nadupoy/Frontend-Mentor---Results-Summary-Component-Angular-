69d5c6fee9c03dca9073604edb62a58a4c2a32a4
nadupoy <gnsampao@gmail.com>
2024-11-21 12:17:50 +0300

    Deleted the button component.

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,35 +1,13 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../button/button.component';
 import { ScoreComponent } from '../score/score.component';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [ButtonComponent, ScoreComponent],
+  imports: [ScoreComponent],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
 export class SummaryComponent {
-//  data = [
-//   {
-//     "category": "Reaction",
-//     "score": 80,
-//     "icon": "../assets/images/icon-reaction.svg"
-//   },
-//   {
-//     "category": "Memory",
-//     "score": 92,
-//     "icon": "../assets/images/icon-memory.svg"
-//   },
-//   {
-//     "category": "Verbal",
-//     "score": 61,
-//     "icon": "../assets/images/icon-verbal.svg"
-//   },
-//   {
-//     "category": "Visual",
-//     "score": 72,
-//     "icon": "../assets/images/icon-visual.svg"
-//   }
-//  ];
+  
 }
